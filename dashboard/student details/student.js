3a0144ce762db8395c91a5ccb258fef651bd893e
Rehan Pasha <104275485+Rehanpasha732@users.noreply.github.com>
2022-11-12 07:19:54 -0800

    Add students details

--- dashboard/student details/student.js
+++ dashboard/student details/student.js
@@ -17,14 +17,43 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore()
 // Initialize Firebase
 
+let contain = document.getElementById("contain")
 window.onload = async() => {
-
     const querySnapshot = await getDocs(collection(db, "Studends"));
     querySnapshot.forEach((doc) => {
         // doc.data() is never undefined for query doc snapshots
         console.log(doc.id, " => ", doc.data());
-
-        console.log(doc.data().studentname)
+        let div = `
+        <div class="card">
+                <div class="img">
+                    <img src="https://img.freepik.com/free-vector/illustration-user-avatar-icon_53876-5907.jpg?w=740&t=st=1668262795~exp=1668263395~hmac=865617d225fe7d6915f3243e238414806bcf4930d3b031c6257203c58ffb796d" alt="">
+                </div>
+                <div class="data">
+                    <div class="text">
+                        <div class="text_name" id="name">Name</div>
+                        <div class="text_data">  ${doc.data().studentname}</div>
+                    </div>
+                    <div class="text">
+                        <div class="text_name">Father Name</div>
+                        <div class="text_data">  ${doc.data().fathername}</div>
+                    </div>
+                    <div class="text">
+                        <div class="text_name">Contact</div>
+                        <div class="text_data">  ${doc.data().studentphone}</div>
+                    </div>
+                    <div class="text">
+                        <div class="text_name">Email</div>
+                        <div class="text_data">  ${doc.data().studentemail}</div>
+                    </div>
+                    <div class="status">
+                        <div class="text_status">Status</div>
+                        <div class="text_data">✔</div>
+                    </div>
+                   
+                </div>
+            </div>
+        `
+        contain.innerHTML += div
     });
 
 }
